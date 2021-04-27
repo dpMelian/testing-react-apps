e6f5a1ea96612de54c0e1db6e54de7bc04872c92
dpMelian <astrodaniel1234@gmail.com>
2021-04-27 11:56:02 +0100

    Fourth and fifth exercise finished

--- src/__tests__/exercise/03.js
+++ src/__tests__/exercise/03.js
@@ -7,15 +7,15 @@ import Counter from '../../components/counter';
 import userEvent from "@testing-library/user-event";
 
 test('counter increments and decrements when the buttons are clicked', () => {
-  render(<Counter />);
-  const decrement = screen.getByRole("button", {name: "Decrement"});
-  const increment = screen.getByRole("button", {name: "Increment"});
+	render(<Counter />);
+	const decrement = screen.getByRole("button", {name: "Decrement"});
+	const increment = screen.getByRole("button", {name: "Increment"});
 
-  const message = screen.getByText("Current count: 0");
+	const message = screen.getByText("Current count: 0");
 
-  expect(message).toHaveTextContent('Current count: 0');
-  userEvent.click(increment);
-  expect(message).toHaveTextContent('Current count: 1');
-  userEvent.click(decrement);
-  expect(message).toHaveTextContent('Current count: 0');
+	expect(message).toHaveTextContent('Current count: 0');
+	userEvent.click(increment);
+	expect(message).toHaveTextContent('Current count: 1');
+	userEvent.click(decrement);
+	expect(message).toHaveTextContent('Current count: 0');
 })
--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -2,11 +2,12 @@
 // http://localhost:3000/login-submission
 
 import * as React from 'react'
-// 🐨 you'll need to grab waitForElementToBeRemoved from '@testing-library/react'
+import {waitForElementToBeRemoved} from '@testing-library/react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import {build, fake} from '@jackfranklin/test-data-bot'
-// 🐨 you'll need to import rest from 'msw' and setupServer from msw/node
+import {rest} from 'msw'
+import {setupServer} from 'msw/node'
 import Login from '../../components/login-submission'
 
 const buildLoginForm = build({
@@ -16,33 +17,79 @@ const buildLoginForm = build({
   },
 })
 
-// 🐨 get the server setup with an async function to handle the login POST request:
-// 💰 here's something to get you started
-// rest.post(
-//   'https://auth-provider.example.com/api/login',
-//   async (req, res, ctx) => {},
-// )
-// you'll want to respond with an JSON object that has the username.
-// 📜 https://mswjs.io/
+const {username, password} = buildLoginForm()
 
-// 🐨 before all the tests, start the server with `server.listen()`
-// 🐨 after all the tests, stop the server with `server.close()`
+const server = setupServer(
+  rest.post(
+    'https://auth-provider.example.com/api/login',
+    async (req, res, ctx) => {
+      if(!req.body.password && req.body.username){
+        return res(ctx.status(400), ctx.json({message: 'password required'}))
+      }
+      if(!req.body.username && req.body.password){
+        return res(ctx.status(400), ctx.json({message: 'username required'}))
+      }
+      if(!req.body.username && !req.body.password){
+        return res(ctx.status(400), ctx.json({message: 'username and password required'}))
+      }
+      return res(ctx.json({username: req.body.username}))
+    }
+  ),
+)
 
-test(`logging in displays the user's username`, async () => {
+beforeAll(() => server.listen())
+afterAll(() => server.close())
+
+test(`if logging in displays the user's username`, async () => {
   render(<Login />)
-  const {username, password} = buildLoginForm()
 
   userEvent.type(screen.getByLabelText(/username/i), username)
   userEvent.type(screen.getByLabelText(/password/i), password)
-  // 🐨 uncomment this and you'll start making the request!
-  // userEvent.click(screen.getByRole('button', {name: /submit/i}))
 
-  // as soon as the user hits submit, we render a spinner to the screen. That
-  // spinner has an aria-label of "loading" for accessibility purposes, so
-  // 🐨 wait for the loading spinner to be removed using waitForElementToBeRemoved
-  // 📜 https://testing-library.com/docs/dom-testing-library/api-async#waitforelementtoberemoved
+  userEvent.click(screen.getByRole('button', {name: /submit/i}))
 
-  // once the login is successful, then the loading spinner disappears and
-  // we render the username.
-  // 🐨 assert that the username is on the screen
+  await waitForElementToBeRemoved(() => 
+    screen.getByLabelText(/loading.../i)
+  )
+  expect(screen.getByText(username)).toBeInTheDocument()
+})
+
+test('if login without password displays error', async () => {
+  render(<Login />)
+
+  userEvent.type(screen.getByLabelText(/username/i), username)
+
+  userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(() =>
+    screen.getByLabelText(/loading.../i)
+  )
+
+  expect(screen.getByRole('alert')).toHaveTextContent('password required')
+})
+
+test('if login without username displays error', async () => {
+  render(<Login />)
+
+  userEvent.type(screen.getByLabelText(/password/i), password)
+
+  userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(() =>
+    screen.getByLabelText(/loading.../i)
+  )
+
+  expect(screen.getByRole('alert')).toHaveTextContent('username required')
+})
+
+test('if login without username and password displays error', async () => {
+  render(<Login />)
+
+  userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(() =>
+    screen.getByLabelText(/loading.../i)
+  )
+
+  expect(screen.getByRole('alert')).toHaveTextContent('username and password required')
 })
